0d977de32680f2f4088841fe86cfa1ed7725daea
itay <itaybella@gmail.com>
2020-10-06 10:29:41 +0300

    adding scalars resolver

--- resolvers/index.js
+++ resolvers/index.js
@@ -2,10 +2,12 @@ const {mergeResolvers} = require('@graphql-tools/merge')
 const authorResolver = require('./authorResolver')
 const commentResolver = require('./commentResolver')
 const postResolver = require('./postResolver')
+const scalarsResolver = require('./scalarsResolver')
 
 module.exports = (authorRepository, commentRepository, postRepository) => mergeResolvers(
     [
         authorResolver(authorRepository),
         commentResolver(commentRepository),
-        postResolver(postRepository)
+        postResolver(postRepository),
+        scalarsResolver
     ])
